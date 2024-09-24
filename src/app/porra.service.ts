df513e2137276d9763ae096ac67c83df2fc19d14
Rodrigo Iglesias Nieto <115586378+iglnierod@users.noreply.github.com>
2024-09-24 20:25:43 +0200

    Cambio tamaño imagenes

--- src/app/porra.service.ts
+++ src/app/porra.service.ts
@@ -24,19 +24,19 @@ export class PorraService {
       nombre: 'Man1a',
       puntos: 4,
       imagenUrl:
-        'https://cdn.discordapp.com/avatars/546473079816192031/11a05c59e2f18c541e8d667f8a673f1c?size=1024',
+        'https://cdn.discordapp.com/avatars/546473079816192031/11a05c59e2f18c541e8d667f8a673f1c?size=60',
     },
     {
       nombre: 'Iker',
       puntos: 5,
       imagenUrl:
-        'https://cdn.discordapp.com/avatars/468839977208512512/13778b4d3d95a3f056c3da80fe14e293?size=1024',
+        'https://cdn.discordapp.com/avatars/468839977208512512/13778b4d3d95a3f056c3da80fe14e293?size=60',
     },
     {
       nombre: 'Alma',
       puntos: 6,
       imagenUrl:
-        'https://cdn.discordapp.com/avatars/735105833112830022/fec2fed592df27512186b8260094d22b?size=1024',
+        'https://cdn.discordapp.com/avatars/735105833112830022/fec2fed592df27512186b8260094d22b?size=60',
     },
   ];
 
