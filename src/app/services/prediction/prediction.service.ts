e3d3dbd7229043b9ff99a2ff2270584a2b8e5598
iglnierod <iglesias.nieto.rodrigo.ald@gmail.com>
2024-09-29 16:39:23 +0200

    Configurados entornos de desarrollo

--- src/app/services/prediction/prediction.service.ts
+++ src/app/services/prediction/prediction.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PredictionService {
-  private apiUrl =
-    'https://porra-champions-backend.onrender.com/api/predictions';
+  private apiUrl = `${environment.apiUrl}/predictions`;
 
   constructor(private http: HttpClient) {}
 
   getPredictions(): Observable<any[]> {
+    console.log(this.apiUrl);
     return this.http.get<any[]>(this.apiUrl);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://porra-champions-backend.onrender.com/api',
+};
